5ce62c5b82b2177d33b4673bfb7f8093c5dd5e2d
hackeru <hackeru@hackeru1.local>
2019-12-25 13:35:51 +0200

    Initial commit

---
+++ app/src/main/java/com/shahar/ofir/lec1part3/MainActivity.java
@@ -0,0 +1,13 @@
+package com.shahar.ofir.lec1part3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
